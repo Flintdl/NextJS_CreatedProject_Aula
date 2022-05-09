9875d44fbe3f1c5c3c9d5d46dedf6fcccee213f3
Diego <51789908+Flintdl@users.noreply.github.com>
2022-05-09 15:52:36 -0300

    Alteração Final
    Subindo o arquivo para deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import GlobalStyle from "../src/theme/GlobalStyle";
+import Head from "next/head";
+
+function HeadPage() {
+  return (
+    <Head>
+      <title>Tutorial NextJS</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="NextJS tutorial" />
+      <html lang="pt-BR" />
+    </Head>
+  );
+}
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <HeadPage />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
